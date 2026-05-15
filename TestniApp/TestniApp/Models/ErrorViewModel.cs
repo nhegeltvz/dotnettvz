e06153f2b7b4778a62b43e2daf17ed68b2cb64a2
Nikola Hegel <nikolahegl@gmail.com>
2026-05-15 08:03:23 +0200

    #main - Added custom date control and forms for CRUD.

---
+++ TestniApp/TestniApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TestniApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
